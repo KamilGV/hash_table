a845b2fc37986b1eef096cb917ae5a10039364c1
user <gkamilv@gmail.com>
2023-11-21 13:26:04 +0500

    feat: add files

---
+++ hash_table.py
@@ -0,0 +1,12 @@
+class HashTable:
+    def __init__(self, *args, **kwargs):
+        self.used = 0
+        self.occupated = 0
+
+    # TODO create
+    # TODO delete
+    # TODO insert one, and few
+    # TODO find by key
+    # TODO update(insert)
+    # TODO check exist key in table
+    # TODO
